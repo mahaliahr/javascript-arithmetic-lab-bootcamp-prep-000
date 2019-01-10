ec60eee1545fc754a536b60a8a4c4bec9f7bf99b
Mahalia Henry-Richards <mahalia_hr@outlook.com>
2019-01-10 12:58:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,22 @@ function add(a,b){
   return a + b
 }
 
+function subtract(a, b){
+  return a - b
+}
+
+function multiply(a, b){
+  return a * b
+}
+
+function divide(a, b){
+  return a / b
+}
+
+function inc(n){
+  n+=
+}
+
 function makeInt(n){
   parseInt(n)
 }
